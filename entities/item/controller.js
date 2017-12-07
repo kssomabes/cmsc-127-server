74893b73073f8aa707d6d994c250c0b970e43e6e
Kia Mei Somabes <kssomabes@up.edu.ph>
2017-12-07 16:55:20 +0800

    minor change in item

--- entities/item/controller.js
+++ entities/item/controller.js
@@ -1,7 +1,7 @@
 const db = require('../../database/connection');
 
 module.exports.showItems = function (callback) {
-	db.query('SELECT * FROM item', (err, rows) => {
+	db.query('SELECT itemCode, name FROM item', (err, rows) => {
 		if (err) callback(err);
 		else callback(null, rows);
 	});
--- entities/item/router.js
+++ entities/item/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 var ctrl = require('./controller');
 
 router.get('/showItems', (req, res) => {
+	console.log('show items');
 	ctrl.showItems((err, rows) => {
 		if (err) res.status(500).json({status: 500, message: 'Server error'});
 		else res.status(200).json({status: 200, data: rows, message: 'Success'});
